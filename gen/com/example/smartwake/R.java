2479e063ea01a5b9277532e7a546b94d080cbfbf
Pablo Moreno Pina <pmorenopina@gmail.com>
2015-11-03 14:59:33 +0100

    Activity Tiempo 90%
    Falta que cambie el icono respecto al tiempo. Y que coja el tiempo de la
    API.

--- gen/com/example/smartwake/R.java
+++ gen/com/example/smartwake/R.java
@@ -41,21 +41,28 @@ public final class R {
     public static final class id {
         public static final int imageView1=0x7f080002;
         public static final int imageView2=0x7f080004;
-        public static final int imageView3=0x7f080005;
         public static final int imageView4=0x7f080006;
         public static final int imageView5=0x7f080007;
+        public static final int imageWeather=0x7f080008;
         public static final int linearLayout=0x7f080000;
         public static final int tableLayout=0x7f080003;
+        public static final int textWeather1=0x7f080009;
+        public static final int textWeather2=0x7f08000a;
+        public static final int textWeather3=0x7f08000b;
+        public static final int textWeather4=0x7f08000c;
         public static final int texto=0x7f080001;
+        public static final int weather=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_weather=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int weatherTitle=0x7f050002;
         public static final int youwillwakeup=0x7f050001;
     }
     public static final class style {
