9c0ca8c0835fcf68735f70890a9036b36bcdff51
Pablo Moreno Pina <pmorenopina@gmail.com>
2015-11-13 13:34:47 +0100

    Mix General , Alarm y Settings

--- src/com/example/smartwake/MainActivity.java
+++ src/com/example/smartwake/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.smartwake;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 public class MainActivity extends Activity{
 
     private ImageView icon;
+    AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+    private TimePicker alarmTimePicker;
+    private static MainActivity inst;
+    private TextView alarmTextView;
+
+    public static MainActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
 
     public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -31,6 +54,11 @@ public class MainActivity extends Activity{
         else{
             Toast.makeText(getBaseContext(), "No tienes conexión!", Toast.LENGTH_LONG).show();
         }
+
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+        //alarmTextView = (TextView) findViewById(R.id.alarmText);
+        ToggleButton alarmToggle = (ToggleButton) findViewById(R.id.alarmToggle);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
     }
 	
 	public void getWeather(View view){
@@ -62,5 +90,36 @@ public class MainActivity extends Activity{
         }
         return false;
     }
+
+    public void onToggleClicked(View view) {
+        if (((ToggleButton) view).isChecked()) {
+            Log.d("MyActivity", "Alarm On");
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+            Intent myIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, 0);
+            alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+        } else {
+            alarmManager.cancel(pendingIntent);
+            setAlarmText("");
+            Log.d("MyActivity", "Alarm Off");
+        }
+    }
+
+    public void setAlarmText(String alarmText) {
+        alarmTextView.setText(alarmText);
+    }
+
+    public void getClock(View view){
+        Intent intent = new Intent(this, ClockActivity.class);
+        startActivity(intent);
+    }
+
+    public void getSettings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
 
 }
